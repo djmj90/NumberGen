aceab752e274b9e370da10ac7b36b86e2b4b4c30
OMEN <OMEN@LAPTOP-R2A57T5L>
2021-03-07 11:36:08 +0300

    remove decimal points

--- NumberGen/Form1.cs
+++ NumberGen/Form1.cs
@@ -49,23 +49,11 @@ namespace NumberGen
             {
                 e.Handled = true;
             }
-
-            // only allow one decimal point
-            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
-            {
-                e.Handled = true;
-            }
         }
 
         private void tbLimit_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
-                e.Handled = true;
-            }
-
-            // only allow one decimal point
-            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
             {
                 e.Handled = true;
             }
