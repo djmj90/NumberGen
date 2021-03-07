af807de490fda4c60bc23a7d1ca539150f7feb6a
OMEN <OMEN@LAPTOP-R2A57T5L>
2021-03-07 11:32:54 +0300

    Adding Creator, textbox only takes integer and Design Modification

--- NumberGen/Form1.Designer.cs
+++ NumberGen/Form1.Designer.cs
@@ -35,21 +35,23 @@ namespace NumberGen
             this.tbLimit = new System.Windows.Forms.TextBox();
             this.btnGenerate = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblCreator = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbStart
             // 
             this.tbStart.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.tbStart.Location = new System.Drawing.Point(116, 17);
+            this.tbStart.Location = new System.Drawing.Point(132, 23);
             this.tbStart.Name = "tbStart";
             this.tbStart.Size = new System.Drawing.Size(150, 24);
             this.tbStart.TabIndex = 0;
+            this.tbStart.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbStart_KeyPress);
             // 
             // lblStart
             // 
             this.lblStart.AutoSize = true;
             this.lblStart.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.lblStart.Location = new System.Drawing.Point(19, 20);
+            this.lblStart.Location = new System.Drawing.Point(35, 26);
             this.lblStart.Name = "lblStart";
             this.lblStart.Size = new System.Drawing.Size(91, 17);
             this.lblStart.TabIndex = 1;
@@ -59,7 +61,7 @@ namespace NumberGen
             // 
             this.lblLimit.AutoSize = true;
             this.lblLimit.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.lblLimit.Location = new System.Drawing.Point(19, 58);
+            this.lblLimit.Location = new System.Drawing.Point(35, 64);
             this.lblLimit.Name = "lblLimit";
             this.lblLimit.Size = new System.Drawing.Size(85, 17);
             this.lblLimit.TabIndex = 3;
@@ -68,17 +70,18 @@ namespace NumberGen
             // tbLimit
             // 
             this.tbLimit.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.tbLimit.Location = new System.Drawing.Point(116, 55);
+            this.tbLimit.Location = new System.Drawing.Point(132, 61);
             this.tbLimit.Name = "tbLimit";
             this.tbLimit.Size = new System.Drawing.Size(150, 24);
             this.tbLimit.TabIndex = 2;
+            this.tbLimit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbLimit_KeyPress);
             // 
             // btnGenerate
             // 
             this.btnGenerate.Font = new System.Drawing.Font("Tahoma", 10F);
             this.btnGenerate.Location = new System.Drawing.Point(22, 97);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(244, 33);
+            this.btnGenerate.Size = new System.Drawing.Size(277, 33);
             this.btnGenerate.TabIndex = 4;
             this.btnGenerate.Text = "Generate";
             this.btnGenerate.UseVisualStyleBackColor = true;
@@ -88,22 +91,36 @@ namespace NumberGen
             // 
             this.progressBar1.Location = new System.Drawing.Point(22, 136);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(244, 23);
+            this.progressBar1.Size = new System.Drawing.Size(277, 23);
             this.progressBar1.TabIndex = 5;
             this.progressBar1.Visible = false;
+            // 
+            // lblCreator
+            // 
+            this.lblCreator.AutoSize = true;
+            this.lblCreator.ForeColor = System.Drawing.Color.Silver;
+            this.lblCreator.Location = new System.Drawing.Point(213, 164);
+            this.lblCreator.Name = "lblCreator";
+            this.lblCreator.Size = new System.Drawing.Size(86, 13);
+            this.lblCreator.TabIndex = 6;
+            this.lblCreator.Text = "created by: djmj";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 171);
+            this.ClientSize = new System.Drawing.Size(311, 186);
+            this.Controls.Add(this.lblCreator);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.lblLimit);
             this.Controls.Add(this.tbLimit);
             this.Controls.Add(this.lblStart);
             this.Controls.Add(this.tbStart);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Number Generator (Liquid)";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -118,6 +135,7 @@ namespace NumberGen
         private System.Windows.Forms.TextBox tbLimit;
         private System.Windows.Forms.Button btnGenerate;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblCreator;
     }
 }
 
